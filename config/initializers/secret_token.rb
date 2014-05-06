ab8c0bb50d90c8af50efd719dbdb2037efcae778
adeng21 <adeng21@gmail.com>
2014-05-06 12:29:24 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dogstagram::Application.config.secret_key_base = '3d5268ba12a82ab990ae2d08f98a9830fd6761a912cb3fbf9d69f35fac9ecfbbf9849a8354a1a5e628fb6ec4b53c621bfd47ffaf0b7aeeeb60b2c32f9fec308a'
